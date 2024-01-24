c3b8b135d74779741138e990ea164e0b7d1a7a38
Phil Peraza <phil.peraza@outlook.com>
2024-01-24 03:41:25 -0800

    feat/ added header markup/layout

---
+++ src/Components/Apps/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className='header'>
+        <div className='header__logo'>
+          <div><img src='/images/Logo.svg' alt='Logo'/></div>
+          <div>Time</div>
+        </div>
+        <div className='header__avatar-logo'>
+          <div><button type='text'>+ Add clothes</button></div>
+          <div>Name</div>
+          <div><img src='/images/avatarlogo.svg' alt='Avatar'/></div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
