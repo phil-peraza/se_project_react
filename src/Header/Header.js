e96fd4cdde41f41961a6f6d840a5fd022f051dea
Phil Peraza <phil.peraza@outlook.com>
2024-02-06 02:18:46 -0800

    fix/ reorganized folder locations on project

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,13 +5,13 @@ const Header = () => {
         <div>
       <header className='header'>
         <div className='header__logo'>
-          <div><img src='/images/Logo.svg' alt='Logo'/></div>
+          <div><img src={require('../images/Logo.svg').default} alt='Logo'/></div>
           <div>Time</div>
         </div>
         <div className='header__avatar-logo'>
           <div><button type='text'>+ Add clothes</button></div>
           <div>Name</div>
-          <div><img src='/images/avatarlogo.svg' alt='Avatar'/></div>
+          <div><img src={require('../images/avatarlogo.svg').default} alt='Avatar'/></div>
         </div>
       </header>
     </div>
