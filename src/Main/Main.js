bbb1bbaa8126dfdf30b3fa630760743688cb3fa6
Phil Peraza <phil.peraza@outlook.com>
2024-02-14 01:49:45 -0800

    feat/ refactor Main.js css code block

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,6 +1,7 @@
 import { defaultClothingItems } from "../Utils/constants";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
+import './Main.css';
 
 function Main({ weatherTemp }) {
     return <main className='main'>
