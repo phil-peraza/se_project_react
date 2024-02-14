fcd10163e0876cb7fd865530b97703fa2e99749b
Phil Peraza <phil.peraza@outlook.com>
2024-02-14 01:31:18 -0800

    feat/ refactor code

---
+++ src/Main/Main.js
@@ -0,0 +1,19 @@
+import { defaultClothingItems } from "../Utils/constants";
+import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+
+function Main({ weatherTemp }) {
+    return <main className='main'>
+      <WeatherCard day={false} type='moony' weatherTemp={weatherTemp} />
+      <section className='card_section' id='card-section'>
+        Today is {weatherTemp} / You may want to wear:
+        <div className='card_items'>
+          {defaultClothingItems.map((x) => (
+            <ItemCard x={x} />
+          ))}
+        </div>
+      </section>
+    </main>;
+  }
+
+  export default Main;
