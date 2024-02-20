81fa865922948735e37b5d23803f0327478aeb64
Phil Peraza <phil.peraza@outlook.com>
2024-02-20 02:04:44 -0800

    feat/ add modal

--- src/Apps/App.js
+++ src/Apps/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import ModalWithForm from '../ModalWithForm/ModalWithForm';
 
 const weatherTemp = '69°F';
 
@@ -11,6 +12,9 @@ function App() {
     <Header />
     <Main weatherTemp={ weatherTemp }/>
     <Footer />
+    <ModalWithForm title='New Garment'>
+      These are the children
+    </ModalWithForm>
     </div>
   );
 }
