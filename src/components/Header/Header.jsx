53344ad6b214a22402fa2e704f6fbed396b41857
Phil Peraza <phil.peraza@outlook.com>
2024-06-27 01:09:31 -0700

    add/header mark-up

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header className='header'>
+            <img className='header__logo' />
+            <p className="header__date-and-location">DATE, LOCATION</p>
+        </header>
+    );
+}
+
+export default Header;
