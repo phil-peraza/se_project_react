bfc908e7e508ba3219323d5dfc4b91bfc2e6aeff
Phil Peraza <phil.peraza@outlook.com>
2024-06-27 01:57:08 -0700

    feat/add basic header markup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
+import "./Header.css"
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.png";
+
 function Header() {
     return (
-        <header className='header'>
-            <img className='header__logo' />
+        <header className="header">
+            <img className="header__logo" src={logo}/>
             <p className="header__date-and-location">DATE, LOCATION</p>
+            <button className="header__add-clothes-btn">+ Add clothes</button>
+            <div className="header__user-container">
+                <p className="header__username">Felipe Peraza</p>
+                <img src={avatar} alt="Felipe Peraza" className="header__avatar" />
+            </div>
         </header>
     );
 }
