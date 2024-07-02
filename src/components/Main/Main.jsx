a18618f6d66ccd9ceddd17f8e5ddee3406f9d631
Phil Peraza <phil.peraza@outlook.com>
2024-07-02 02:30:58 -0700

    feat/ add item cards to page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,22 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
 
 function Main() {
     return (
     <main>
         <WeatherCard />
-        <section>
+        <section className="cards">
             <p className="cards__text">
                 Today is 75&deg; F / You may want to wear:
             </p>
-            {/* TODO - add the cards */}
+            <ul className="cards__list">
+                {defaultClothingItems.map((item) => {
+                    return (
+                       <ItemCard key={item._id} item={item} />
+                    );
+                })}
+            </ul>
         </section>
     </main>
     );
