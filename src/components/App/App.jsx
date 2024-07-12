640e071630f3f5ac1b06110c7d971a379de8b001
Phil Peraza <phil.peraza@outlook.com>
2024-07-12 01:27:02 -0700

    fix/modalWithForm refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,7 +15,53 @@ function App() {
         <Header />
         <Main weatherData={weatherData} />
       </div>
-      <ModalWithForm />
+      <ModalWithForm title="New garment" buttonText="Add garment">
+      <label htmlFor="name" className="modal__lable">
+                    Name 
+                    <input 
+                    type="text" 
+                    className="modal__input" 
+                    id="name" 
+                    placeholder="Name"
+                    />
+                </label>
+                <label htmlFor="imageUrl" className="modal__lable">
+                    Image
+                    <input 
+                    type="text" 
+                    className="modal__input" 
+                    id="imageUrl" 
+                    placeholder="Image URL"
+                    />
+                </label>
+                <fieldset className="modal__radio-buttons">
+                    <legend className="modal__legend">Select the weather type:</legend>
+                    <label htmlFor="hot" className="modal__label modal__label_type_radio">
+                        <input 
+                            id="hot"
+                            type="radio" 
+                            className="modal__radio-input" 
+                        />
+                        Hot
+                    </label>
+                    <label htmlFor="warm" className="modal__label modal__label_type_radio">
+                        <input
+                            id="warm"
+                            type="radio" 
+                            className="modal__radio-input" 
+                        />
+                        Warm
+                    </label>
+                    <label htmlFor="cold" className="modal__label modal__label_type_radio">
+                        <input 
+                            id="cold"
+                            type="radio" 
+                            className="modal__radio-input" 
+                        />
+                        Cold
+                    </label>
+                </fieldset>
+      </ModalWithForm>
     </div>
   );
 }
