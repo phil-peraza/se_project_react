b7ca9a3651d93b26fcaab0e083dec2ee38de27df
Phil Peraza <phil.peraza@outlook.com>
2024-07-16 02:41:12 -0700

    feat/ add current date functionality to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,15 @@ import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 
 function Header({ handleAddClick }) {
+    const currentDate = new Date().toLocaleString('default', { 
+        month: 'long', 
+        day: 'numeric' 
+    });
+
     return (
         <header className="header">
             <img className="header__logo" src={logo}/>
-            <p className="header__date-and-location">DATE, LOCATION</p>
+            <p className="header__date-and-location">{ currentDate }</p>
             <button 
             onClick={handleAddClick}
             type="button" 
