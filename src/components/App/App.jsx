464c76cbc95143f0359c29c1c98fe1627e219aaa
Felipe Peraza <phil.peraza@outlook.com>
2024-07-23 16:49:55 -0700

    fix/ modal window content

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -71,7 +71,7 @@ function App() {
                     />
                 </label>
                 <fieldset className="modal__radio-buttons">
-                    <legend className="modal__legend">Select the fit type:</legend>
+                    <legend className="modal__legend">Select the weather type:</legend>
                     <label htmlFor="hot" className="modal__label modal__label_type_radio">
                         <input 
                             id="hot"
