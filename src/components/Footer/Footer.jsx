cd79dbe0b2cff3d1c3f7336d5fbf33e79936ebe6
Felipe Peraza <phil.peraza@outlook.com>
2024-11-13 16:21:09 -0800

    feat/ added footer markup and layout

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <section className="footer">
+            <p className="footer__name">
+                Developed by Felipe Peraza
+            </p>
+            <p className="footer__year">
+                2024
+            </p>
+        </section>
+    );
+}
+
+export default Footer;
