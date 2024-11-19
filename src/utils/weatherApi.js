420a40d6ba9f97b7145132d4cf25bdce8a76ebe8
Felipe Peraza <phil.peraza@outlook.com>
2024-11-19 01:35:07 -0800

    fix/ weather card api calls and rendering

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -15,13 +15,19 @@ export const filterWeatherData = (data) => {
     result.city = data.name;
     result.temp = { F: data.main.temp };
     result.type = getWeatherType(result.temp.F);
+    result.condition = data.weather[0].main.toLowerCase();
+    result.isDay = isDay(data.sys, Date.now());
     return result;
 };
+
+const isDay = ({ sunrise, sunset }, now) => {
+    return sunrise * 1000 < now && now < sunset * 1000;
+}
 
 const getWeatherType = (temperature) => {
     if (temperature >= 86) {
         return 'hot';
-      } else if (temperature >= 66) {
+      } else if (temperature >= 66 && temperature < 86) {
         return 'warm';
       } else {
         return 'cold';
