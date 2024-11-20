9c361b89dd076080df458a35fcafb3c51fcec399
Felipe Peraza <phil.peraza@outlook.com>
2024-11-20 12:49:43 -0800

    fix/ remove hardcode in universal component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -50,7 +50,7 @@ function App() {
    <ModalWithForm
     title="New garment"
     buttonText="Add garment"
-    activeModal={activeModal}
+    isOpen={activeModal === "add-garment"}
     onClose={closeActiveModal}
    >
     <label htmlFor="name" className="modal__lable">
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,8 +1,8 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
+function ModalWithForm({ children, buttonText, title, onClose, isOpen }) {
  return (
-  <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
+  <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
    <div className="modal__content">
     <h2 className="modal__title">{title}</h2>
     <button onClick={onClose} type="button" className="modal__close"/>
